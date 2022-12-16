16cb12d4ace17dba7cbfb9aa19883783112aa196
Krzysztof Reczek <reczek.krzysztof@gmail.com>
2022-12-16 22:19:36 +0100

    ...

--- stream/stream.go
+++ stream/stream.go
@@ -5,9 +5,7 @@ package stream
 //
 // In order for your domain type to implement Stream:
 //   - Embed pointer to Events queue.
-//   - Implement `StreamID` returning a unique identifier (usually the same as your stream's internal ID).
-//   - Implement `Events` to expose the Events queue.
-//   - Implement `WithEvents` that returns a new instance with the provided Events queue set.
+//   - Implement the interface methods in accordance with its description.
 //
 // Then an EventStore will be able to store and load it.
 //
@@ -30,8 +28,13 @@ package stream
 //		return &User{events: events}
 //	}
 type Stream[T any] interface {
+	// StreamID returns a unique identifier (usually the same as your stream's internal ID).
 	StreamID() ID
+
+	// Events exposes a pointer to the Events queue.
 	Events() *Events[T]
+
+	// NewFromEvents returns a new instance with the provided Events queue.
 	NewFromEvents(events *Events[T]) *T
 }
 
@@ -42,6 +45,7 @@ func (i ID) String() string {
 	return string(i)
 }
 
+// Record applies a provided Event and puts that into the stream's internal Events queue.
 func Record[T Stream[T]](stream *T, e Event[T]) error {
 	err := e.ApplyTo(stream)
 	if err != nil {
@@ -53,6 +57,9 @@ func Record[T Stream[T]](stream *T, e Event[T]) error {
 	return nil
 }
 
+// New instantiates a new T with all Events applied to it.
+// At the same time the stream's internal Events queue is initialised,
+// so it can record new upcoming events.
 func New[T Stream[T]](eventsSlice []VersionedEvent[T]) (*T, error) {
 	var t T
 

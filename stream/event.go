31eefce4de6e66e00bb7f95f76e9094d34b93d80
Miłosz Smółka <milosz.smolka@gmail.com>
2022-12-30 23:53:00 +0100

    Refactor

---
+++ stream/event.go
@@ -0,0 +1,23 @@
+package stream
+
+// EventName identifies the type of the event and the version of its schema, e.g. "FooCreated_v1".
+type EventName string
+
+type Event[T any] interface {
+	// EventName should identify the event and the version of its schema.
+	//
+	// Example implementation:
+	// 	func (e FooCreated) EventName() EventName {
+	// 		return "FooCreated_v1"
+	// 	}
+	EventName() EventName
+
+	// ApplyTo applies the event to the stream.
+	ApplyTo(*T) error
+}
+
+// VersionedEvent is an event with a corresponding stream version.
+type VersionedEvent[T any] struct {
+	Event[T]
+	StreamVersion int
+}

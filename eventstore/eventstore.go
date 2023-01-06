b830d280eb019afba161999136ab718ea99f7301
Miłosz Smółka <milosz.smolka@gmail.com>
2023-01-06 20:47:11 +0100

    Fix

--- eventstore/eventstore.go
+++ eventstore/eventstore.go
@@ -11,7 +11,7 @@ var ErrEntityNotFound = errors.New("entity not found by ID")
 
 // EventStore loads and saves T implementing esja.Entity.
 type EventStore[T esja.Entity[T]] interface {
-	// Load fetches all events for the stream id and returns a new instance of T based on them.
+	// Load fetches all events for the ID and returns a new instance of T based on them.
 	Load(ctx context.Context, id string) (*T, error)
 
 	// Save saves events recorded in the entity's stream.
